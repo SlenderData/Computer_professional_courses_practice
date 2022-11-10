da22ecd9101b87df0f6732b3ad530b1d504b59d7
SlenderData <mayunji2001@outlook.com>
2022-11-10 09:27:42 +0800

    Update file

--- C_programming/demo_202211.c
+++ C_programming/demo_202211.c
@@ -31,14 +31,14 @@ int test1() // 输出菱形
 
 // 函数练习
 
-float avgfloat(float x, float y)  // 输出两个浮点数的平均值
+float avgfloat(float x, float y) // 输出两个浮点数的平均值
 {
     float avg;
     avg = (x + y) / 2;
     return avg;
 }
 
-int test2()  // 调用上面的求平均值函数
+int test2() // 调用上面的求平均值函数
 {
     float a, b;
     printf("请输入两个浮点数: ");
@@ -46,8 +46,33 @@ int test2()  // 调用上面的求平均值函数
     printf("两个浮点数的平均值为%f\n", avgfloat(a, b));
 }
 
+char change(char x) // 用函数调用完成小写字母转大写字母
+{
+    x = x - 32;
+    return x;
+}
+
+int test3() //调用上面的转换函数
+{
+    char a;
+    do
+    {
+        printf("请输入一个小写字母: ");
+        scanf("%c", &a);
+        if (a < 'a' || a > 'z')
+        {
+            printf("输入有误, 请重新输入!\n");
+        }
+        else
+        {
+            printf("输入正确!\n");
+        }
+    } while (a < 'a' || a > 'z');
+    printf("%c 转换为大写字母后为 %c\n", a, change(a));
+}
+
 int main()
 {
-    test2();
+    test3();
     return 0;
 }
